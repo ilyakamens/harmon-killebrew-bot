a52094e54aa38057efcbac080e6d2a51b0450855
Ilya Kamens <ilyakamens@gmail.com>
2015-04-04 14:33:23 -0400

    make it work

--- harmon_killebrew_bot.py
+++ harmon_killebrew_bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import random
-import re
+import datetime
+import pymysql.cursors
 import signal
 import traceback
 
@@ -10,19 +10,34 @@ from optparse import OptionParser
 from pprint import pprint
 from time import sleep
 
+# terminate process
+def terminate(signum, frame):
+    global stopped
+    stopped = True
+    connection.close()
+    hipchat.send_messages(room_id=room_id, message="killebrew_bot deactivated :(", sender='killebrew_bot')
+signal.signal(signal.SIGTERM, terminate)
+signal.signal(signal.SIGINT, terminate)
+
+def add_word(celeb, said_by):
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO `names` (`celeb`, `said_by`, `date_said`) VALUES (%s, %s, %s)"
+            result = cursor.execute(sql, (celeb, said_by, str(datetime.date.today())))
+
+        connection.commit()
+    except Exception as e:
+        result = str(e.args[0])
+
+    return result
+
 if __name__ == '__main__':
-    stopped = False
-
-    def terminate(signum, frame):
-        global stopped
-        stopped = True
-    signal.signal(signal.SIGTERM, terminate)
-    signal.signal(signal.SIGINT, terminate)
-
-    parser = OptionParser(usage='usage: %prog <api_token> <room_id>')
+    # get arguments
+    parser = OptionParser(usage='usage: %prog <api_token> <room_id> <database>')
     (options, args) = parser.parse_args()
 
-    if len(args) != 2:
+    # make sure we get the right number of args
+    if len(args) != 3:
         parser.print_help()
         exit(1)
 
@@ -30,9 +45,12 @@ if __name__ == '__main__':
     room_id = args[1]
 
     player_ids = set([
-        1355590,  # Ilya Kamens
+        1355590, # Ilya Kamens
+        1022218, # Diggory Rycroft
+        1710903  # Laura Del Beccaro
     ])
 
+    # get users in hipchat room
     while True:
         response = hipchat.get_users_list()
         if response['status'] == 200:
@@ -42,37 +60,45 @@ if __name__ == '__main__':
             sleep(5)
             continue
 
+    # create user map
     players = {}
     for user in users:
         user_id = user['user_id']
         if user_id in player_ids:
             players[user_id] = user
 
-    hipchat.send_messages(room_id=room_id, message='killebrew_bot activated', sender='killebrew_bot')
-    last_date = None
+    # connect to db
+    connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 db=args[2],
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
 
+    # say hi to chat!
+    hipchat.send_messages(room_id=room_id, message='killebrew_bot activated :)', sender='killebrew_bot')
+
+    last_date = None
+    stopped = False
     while not stopped:
         try:
             messages = hipchat.get_messages(room_id=room_id, date='recent')
             for message in messages['data']['messages']:
                 if last_date is not None and message['date'] > last_date:
                     message_text = message['message'].lower()
-                    matches = re.findall('https://github.com/mixpanel/(?:analytics|mixpanel-\w+)/pull/\d+', message_text)
-                    if message['from']['name'] != 'review_bot' and 'review' in message_text and matches:
-                        possibles = player_ids.difference([message['from']['user_id']])
-                        reviewer = players[random.choice(list(possibles))]
-                        if len(matches) == 1:
-                            text = '@%s selected to review %s' % (reviewer['mention_name'], matches[0])
+                    if '(upvote)' in message_text:
+                        celeb = message_text.replace('(upvote)', '').strip()
+                        result = add_word(celeb, message['from']['name'])
+                        if result == '1062':
+                            hipchat.send_messages(room_id=room_id, message='No!', sender='killebrew_bot')
+                        elif isinstance(result, basestring):
+                            hipchat.send_messages(room_id=room_id, message='Error: %s' % result, sender='killebrew_bot')
                         else:
-                            text = '@%s selected to review %d patches: %s' % (reviewer['mention_name'], len(matches), ' and '.join(matches))
-                        hipchat.send_messages(room_id=room_id, message=text, message_format='text', sender='review_bot')
-                    elif message_text.lower() == 'what is my user id?':
-                        hipchat.send_messages(room_id=room_id, message=message['from']['user_id'], message_format='text', sender='user_id_bot')
+                            hipchat.send_messages(room_id=room_id, message='New entry # %s!' % (result), sender='killebrew_bot')
+                    elif message_text == 'what is my user id?':
+                        hipchat.send_messages(room_id=room_id, message=message['from']['user_id'], message_format='text', sender='killebrew_bot')
             last_date = message['date']
         except:
             if 'messages' in locals():
                 pprint(messages)
             print traceback.format_exc()
-        sleep(5)
-
-    hipchat.send_messages(room_id=room_id, message="killebrew_bot deactivated", sender='killebrew_bot')
+        sleep(10)
