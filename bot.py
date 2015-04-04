c6097f4717e2fc15e14ab3404c7b135a79d0e41b
Ilya Kamens <ilyakamens@gmail.com>
2015-04-04 18:44:33 -0400

    make this a little more reusable

--- bot.py
+++ bot.py
@@ -22,11 +22,13 @@ def terminate(signum, frame):
 signal.signal(signal.SIGTERM, terminate)
 signal.signal(signal.SIGINT, terminate)
 
-def add_word(celeb, said_by):
+def add_word(celeb, author):
     try:
         with connection.cursor() as cursor:
-            sql = "INSERT INTO `names` (`celeb`, `said_by`, `date_said`) VALUES (%s, %s, %s)"
-            cursor.execute(sql, (celeb, said_by, str(date.today())))
+            # sql statement needs to be broken out, otherwise it breaks due to dynamic table name
+            sql = "INSERT INTO `%s` " % table_name
+            sql += "(`celeb`, `author`, `authored`) VALUES (%s, %s, %s)"
+            cursor.execute(sql, (celeb, author, str(date.today())))
 
         connection.commit()
     except Exception as e:
@@ -42,12 +44,12 @@ def add_word(celeb, said_by):
             order = -order
             dubdub_text = 'DUB DUB! '
 
-        next_player_index = (player_list.index(said_by) + order) % len(player_list)
+        next_player_index = (player_list.index(author) + order) % len(player_list)
         global current_player
         current_player = player_list[next_player_index]
         send_message('%s"%s" said by %s on %s. Next player is @%s!' % (dubdub_text,
                                                                        celeb,
-                                                                       said_by,
+                                                                       author,
                                                                        str(date.today()),
                                                                        player_map[current_player]['mention_name']))
 
@@ -56,21 +58,31 @@ def send_message(text):
 
 if __name__ == '__main__':
     # get arguments
-    parser = OptionParser(usage='usage: %prog <api_token> <room_id> <database> <userlist.json>')
+    parser = OptionParser(usage='usage: %prog <config_file.json>')
     (options, args) = parser.parse_args()
 
     # make sure we get the right number of args
-    if len(args) != 4:
+    if len(args) != 1:
         parser.print_help()
         exit(1)
 
-    hipchat = Hipster(args[0])
-    room_id = args[1]
+    try:
+        with open(args[0]) as json_config:
+            config = json.load(json_config)
+    except Exception:
+        sys.exit('Failed to open config file: %s' % args[0])
+    else:
+        db_name = config['db_name']
+        table_name = config['table_name']
+        player_list = config['player_list']
+        super_user = string.capwords(config['super_user'])
+        hipchat = Hipster(config['api_key'])
+        room_id = config['room_id']
 
     # connect to db
     connection = pymysql.connect(host='localhost',
                                  user='root',
-                                 db=args[2],
+                                 db=db_name,
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
 
@@ -83,16 +95,6 @@ if __name__ == '__main__':
         else:
             sleep(5)
             continue
-
-    # open user list file
-    try:
-        with open(args[3]) as user_list_file:
-            player_list = json.load(user_list_file)
-    except Exception:
-        connection.close()
-        error_message = 'Failed to open user list file: %s' % args[3]
-        send_message(error_message)
-        sys.exit(error_message)
 
     # create user map
     player_map = {}
@@ -114,14 +116,14 @@ if __name__ == '__main__':
             for message in messages['data']['messages']:
                 if last_date is not None and message['date'] > last_date:
                     message_text = message['message'].lower()
-                    said_by = message['from']['name']
-                    if '(upvote)' in message_text and current_player == said_by:
+                    author = message['from']['name']
+                    if '(upvote)' in message_text and current_player == author:
                         celeb = message_text.replace('(upvote)', '').strip()
-                        add_word(celeb, said_by)
-                    elif message_text == 'reverse order' and said_by == 'Ilya Kamens':
+                        add_word(celeb, author)
+                    elif message_text == 'reverse order' and author == super_user:
                         order = -order
                         send_message('Order reversed')
-                    elif 'set current player:' in message_text and said_by == 'Ilya Kamens':
+                    elif 'set current player:' in message_text and author == super_user:
                         current_player = string.capwords(message_text.replace('set current player:', '').strip())
                         send_message('Current player is @%s' % player_map[current_player]['mention_name'])
 
